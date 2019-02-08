649b788c018a1f9f7754da0d7d60dd70093eb997
vijay <contact@vijaykumar.me>
2019-02-08 15:11:52 -0600

    added unit tets

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,33 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Add_WorksWithTwoNegativeIntegers()
         {
+            var expected = true;
+            var a = -5;
+            var b = 6;
+            var actual = IsSumPositive(a, b);
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [InlineData(true, -2, 2)]
+        [InlineData(false, -7, -5)]
+        [InlineData(false, -9, 5)]
+        public void Add_SumIsPositive(bool exp, int n1, int n2)
+        {
+            Assert.Equal(exp, IsSumPositive(n1, n2));
+        }
 
+        bool IsSumPositive(int n1, int n2)
+        {
+            if (n1 + n2 >= 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 }

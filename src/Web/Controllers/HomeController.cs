b20a631bfa3bc10a99a108e6d85fbc91f37b1b4f
vijay <contact@vijaykumar.me>
2019-02-08 14:39:20 -0600

    added new action on HomeController

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -20,6 +20,12 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
